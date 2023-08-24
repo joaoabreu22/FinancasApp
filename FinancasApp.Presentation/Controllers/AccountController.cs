7c70d4c3b488dd1f388d3bd7b28b5020bdc08a7b
João Abreu <abreunegocio@gmail.com>
2023-08-23 21:47:44 -0300

    first commit

---
+++ FinancasApp.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinancasApp.Presentation.Controllers
+{
+    /// <summary>
+    /// Classe de controle do Asp.Net MVC
+    /// </summary>
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Método para abrir a página /Account/Login
+        /// </summary>
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassoword()
+        {
+            return View();
+        }
+    }
+}
