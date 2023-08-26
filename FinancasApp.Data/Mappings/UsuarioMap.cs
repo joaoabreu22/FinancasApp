1c392ed8d871888fc1ac1eea5535d62e437418bd
João Abreu <abreunegocio@gmail.com>
2023-08-25 21:49:33 -0300

    feat: desenvolvimento do repositorio com EF

---
+++ FinancasApp.Data/Mappings/UsuarioMap.cs
@@ -0,0 +1,45 @@
+﻿using FinancasApp.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FinancasApp.Data.Mappings
+{
+    public class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            //nome da tabela
+            builder.ToTable("USUARIO");
+
+            //chave primary da tabela
+            builder.HasKey(u => u.Id);
+
+            //mapeamento dos campos
+            builder.Property(u => u.Id)
+                .HasColumnName("ID")
+                .IsRequired();
+
+            builder.Property(u => u.Nome)
+                .HasColumnName("NOME")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(u => u.Email)
+                .HasColumnName("EMAIL")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder.Property(u => u.Senha)
+                .HasColumnName("SENHA")
+                .HasMaxLength(40)
+                .IsRequired();
+
+            builder.Property(u => u.DataHoraCriacao)
+                .HasColumnName("DATAHORACRIACAO")
+                .IsRequired();
+        }
+    }
+}
---
+++ FinancasApp.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,67 @@
+﻿using FinancasApp.Data.Contexts;
+using FinancasApp.Data.Entities;
+
+namespace FinancasApp.Data.Repositories
+{
+    public class UsuarioRepository
+    {
+        public void Create(Usuario usuario)
+        {
+            using (var dataContext = new DataContext())
+            {
+                dataContext.Add(usuario);
+                dataContext.SaveChanges();
+            }
+        }
+
+        public void Update(Usuario usuario)
+        {
+            using (var dataContext = new DataContext())
+            {
+                dataContext.Update(usuario);
+                dataContext.SaveChanges();
+            }
+        }
+
+        public void Delete(Usuario usuario)
+        {
+            using (var dataContext = new DataContext())
+            {
+                dataContext.Remove(usuario);
+                dataContext.SaveChanges();
+            }
+        }
+
+        public List<Usuario> GetAll(Usuario usuario)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext.Usuario.OrderBy(u => u.Nome).ToList();
+            }
+        }
+
+        public Usuario? GetUsuario(Guid Id)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext.Usuario.Where(u => u.Id == Id).FirstOrDefault();
+            }
+        }
+
+        public Usuario? GetByEmail(string Email)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext.Usuario.Where(u => u.Email == Email).FirstOrDefault();
+            }
+        }
+
+        public Usuario? GetByEmailAndSenha(string Email, string Senha)
+        {
+            using (var dataContext = new DataContext())
+            {
+                return dataContext.Usuario.Where(u => u.Email == Email && u.Senha == Senha).FirstOrDefault();
+            }
+        }
+    }
+}
